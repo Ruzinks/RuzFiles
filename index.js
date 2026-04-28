ac07ee275682197d2974c76692c721720394986a
Ruzinks <bautikravsoski@gmail.com>
2026-04-28 18:24:27 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require("uuid");
 const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Servir frontend
 app.use(express.static("public"));
